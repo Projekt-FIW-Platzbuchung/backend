0a34efe30990f9820dd59de3bc7852ea24b05cec
Cindy St <93219954+CinSt4@users.noreply.github.com>
2024-11-07 11:30:20 +0100

    schema geaendert

--- models/buchungen.js
+++ models/buchungen.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
 
-const gebuchterPlatzSchema = new mongoose.Schema({
+const buchungenSchema = new mongoose.Schema({
     UserId: { type: Number, ref: 'User', required: true }, 
     PlatzId: { type: Number, ref: 'Platz', required: true }, 
     datum: { type: Date, required: true } 
 });
 
-const buchungenSchema = new mongoose.Schema({
-    BuchungsId: { type: Number, required: true }, 
-    GebuchtePlätze: [gebuchterPlatzSchema] 
-});
 
 
 module.exports = mongoose.model('Buchungen', buchungenSchema, 'buchungen');
