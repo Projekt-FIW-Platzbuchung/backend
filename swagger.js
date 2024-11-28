85f1c0c70a6ae18d86c41ecae447db98c722a220
Melisa Cihan <melisa-cihan@outlook.de>
2024-11-28 10:31:12 +0100

    create+implement swagger file

---
+++ swagger.js
@@ -0,0 +1,29 @@
+// swagger.js
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'SD&C Datac!ders Seat Booking System',
+      version: '1.0.0',
+      description: 'API documentation for your Node.js project',
+    },
+    servers: [
+      {
+        url: 'http://localhost:4000', // Update with your server's URL
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Path to your route files
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+const setupSwaggerDocs = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  console.log('Swagger docs available at http://localhost:4000/api-docs');
+};
+
+module.exports = setupSwaggerDocs;
