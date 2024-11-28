ddd0f69a04c83811330751a7266f29a601543371
Melisa Cihan <melisa-cihan@outlook.de>
2024-11-28 13:51:01 +0100

    refactor date type to String

--- models/bookings.js
+++ models/bookings.js
@@ -4,7 +4,7 @@ const bookingScheme = new mongoose.Schema({
     //bookingsId: { type: Number, required: true },
     userId: { type: Number, ref: 'user', required: true }, 
     seatId: { type: Number, ref: 'seat', required: true }, 
-    date: { type: Date, required: true } 
+    date: { type: String, required: true } 
 });
 
 
