3047945f529a3174b698755a6154d77fa2222042
Melisa Cihan <melisa-cihan@outlook.de>
2024-11-28 10:54:51 +0100

    delete comment

--- swagger.js
+++ swagger.js
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:4000', // Update with your server's URL
+        url: 'http://localhost:4000', 
       },
     ],
   },
