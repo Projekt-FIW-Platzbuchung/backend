c57282d1bb28608e020b02e44b4998602588a68d
Melisa Cihan <melisa-cihan@outlook.de>
2024-11-28 10:31:49 +0100

    add const Swagger to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const setupSwaggerDocs = require('./swagger');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 
