29099f75630c9488e4d1d97ec194fcc1c1700f4f
karenina4pachy <karenina.schroeder@posteo.de>
2024-12-18 16:19:34 +0100

    pre-hook in bookings model implementiert der doppelte buchungen verhindert, gibt gerade bei nicht-erfolgreicher validierung einen konsolenfehler aus

--- models/bookings.js
+++ models/bookings.js
@@ -1,11 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const bookingScheme = new mongoose.Schema({
-    
-    userId: { type: Number, ref: 'user', required: true }, 
-    username: { type: String, required: true },
-    seatId: { type: Number, ref: 'seat', required: true }, 
-    date: { type: String, required: true } 
+  userId: { type: Number, ref: "user", required: true },
+  username: { type: String, required: true },
+  seatId: { type: Number, ref: "seat", required: true },
+  date: { type: String, required: true },
 });
 
-module.exports = mongoose.model('bookings', bookingScheme, 'bookings');
+bookingScheme.pre("save", async function (next) {
+  const existingBooking = await mongoose.model("bookings").findOne({
+    seatId: this.seatId,
+    date: this.date,
+  });
+
+  if (existingBooking) {
+    message = "Dieser Platz ist an dem angegebenen Datum bereits gebucht.";
+    
+    return next(new Error());
+  }
+
+  next();
+});
+
+module.exports = mongoose.model("bookings", bookingScheme, "bookings");
