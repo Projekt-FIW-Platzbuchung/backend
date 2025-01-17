41052fe637fe59d2773416ba817345626edf564a
elise <elise.schwentesius@student.htw-berlin.de>
2025-01-17 19:12:15 +0100

    merge mit floorplan

--- models/seat.js
+++ models/seat.js
@@ -1,25 +1,12 @@
 const mongoose = require('mongoose');
 
 const seatScheme = new mongoose.Schema({
-    seatId: { type: Number, required: true, unique: true }, // Platz-ID als Zahl
-    coordinates: {
-        x: { type: Number, required: true},
-        y: { type: Number, required: true},
-    },
-    properties: {
-        Table: { type: String, required: false }, // Tisch als String
-        Monitor: { type: String, required: false }, // Monitor als String
-        WindowSeat: { type: String, required: false }, // Fensterplatz Ja/Nein
-        TableType: { type: String, required: false }, // Art des Tisches
-        Accessibility: { type: String, required: false }, // Rollstuhlgerecht/Nicht Rollstuhlgerecht
-        Acoustics: { type: String, required: false }, // Lärmisolierung vorhanden/nicht vorhanden
-        WorkTop: { type: String, required: false }, // Standard/Erweitert
-        Chair: { type: String, required: false } // Art des Stuhls
-        //hdmi kabel
-        //monitor defekt
-        //notizenfeld
-    }
-    
+  seatId: { type: Number, required: true, unique: true }, // Platz-ID als Zahl
+  properties: { type: Schema.Types.Mixed, default: {} }, // Nutzt den Mixed-Typ für Flexibilität bei den Eigenschaften
+  coordinates: {
+      x: { type: Number, required: true},
+      y: { type: Number, required: true},
+  }
 });
 
 module.exports = mongoose.model('seat', seatScheme, 'seat');
