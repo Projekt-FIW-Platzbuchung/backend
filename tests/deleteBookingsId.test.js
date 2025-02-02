882a5f847085eddbfd61e7020159842117a069d1
Cindy St <93219954+CinSt4@users.noreply.github.com>
2025-02-02 17:50:48 +0100

    add deleteBookingsId test

---
+++ tests/deleteBookingsId.test.js
@@ -0,0 +1,55 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const app = require('../server'); // Importieren Sie Ihre App-Instanz von Ihrer Hauptserverdatei
+const Booking = require('../models/bookings');
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+ 
+  await mongoose.disconnect();
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  await Booking.insertMany([
+    { seatId: 1, date: '2024-10-10', userId: 1, username: 'Alice', coordinates: { x: 10, y: 15 } },
+  ]);
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+
+describe("DELETE /bookings/:id", () => {
+
+  it("should delete a booking with a valid id and return status 204", async () => {
+    const booking = await Booking.findOne({ seatId: 1 });
+
+    const response = await request(app)
+      .delete(`/bookings/${booking._id}`)
+      .set('Accept', 'application/json') 
+      .expect(204); 
+
+    
+    const deletedBooking = await Booking.findById(booking._id);
+    expect(deletedBooking).toBeNull(); 
+  });
+
+  it("should return 404 for a non-existent booking id", async () => {
+    
+    const fakeId = new mongoose.Types.ObjectId();
+    const response = await request(app)
+      .delete(`/bookings/09349995454444440987645342`) 
+      .set('Accept', 'application/json') 
+      .expect(404); 
+
+    
+    expect(response.body.error).toBe("Buchung nicht gefunden");
+  });
+});
